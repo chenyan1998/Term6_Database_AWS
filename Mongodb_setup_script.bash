5b432b136e73124d3cb431525845e2cfcae63c0a
chiziheng <40351172+chiziheng@users.noreply.github.com>
2020-12-02 20:11:36 +0800

    Update Mongodb_setup_script.bash

--- Mongodb_setup_script.bash
+++ Mongodb_setup_script.bash
@@ -16,10 +16,11 @@ sleep 5 #wait for restart to finish
 
 echo "Downloading kindle metadata"
 #wget -c https://istd50043.s3-ap-southeast-1.amazonaws.com/meta_kindle_store.zip -O meta_kindle_store.zip
-wget -c https://github.com/chiziheng/ziheng-s-first-repo/blob/main/kindle_metadata_final.zip?raw=true --output-document=kindle_metadata_final.zip
+#wget -c https://github.com/chiziheng/ziheng-s-first-repo/blob/main/kindle_metadata_final.zip?raw=true --output-document=kindle_metadata_final.zip
+https://raw.githubusercontent.com/chenyan1998/50043.DataBase_And_BigData/main/kindle_metadata_final.json?token=AJT3LRAUS3WKGFQTU56TKES7Y6CGS
 
-echo "Unzipping metadata file"
-unzip kindle_metadata_final.zip
+#echo "Unzipping metadata file"
+#unzip kindle_metadata_final.zip
 
 echo "Downloading mongodb commands and execute"
 wget https://raw.githubusercontent.com/chenyan1998/50043.DataBase_And_BigData/main/mongo_commands.js?token=AJT3LRB3VQ3K3BTUIWZ2FF27Y6B3K
