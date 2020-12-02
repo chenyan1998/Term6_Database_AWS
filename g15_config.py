b772acdc1b8bd985acba7a83231629d48954d989
sutdorg <hua@HUA-LAPTOP-DEB.HUA-LAPTOP-DEB>
2020-12-02 16:04:08 +0800

    fix

--- g15_config.py
+++ g15_config.py
@@ -1,22 +1,48 @@
-class sg_ids():
+class sg_ids:
+    """a class to store security group ids"""
+
     def __init__(self):
         self.web = ''
         self.mysql = ''
         self.mongo = ''
         self.hadoop = ''
 
-CRED = """aws_access_key_id=ASIAQ6R4KCM7XHJQRE7D
-aws_secret_access_key=mwJOkmsmQGhCZ/n/DKw1Tu6TJ1MkQrL8vW+s05wc
-aws_session_token=FwoGZXIvYXdzECQaDCCy432U0oi7XCY79iLOAbJu4gPkxOkiND/zXkpXE9LKXAHyUOjbdgCZNXQ2KoJd9iNYBm1ZSDxXFThqi7voavN5ygXHUt6nSwtgayCP1+pq+vQwRMDYdjxWOHnxtYrHTXizJ6ZPa5CWCMSHVfFnYKApLtX368zRmP2+G8ZL7Nfk6Mf+Av2RDUR1tG8I/soULUSzBZDbzH4shwM4nkwlC6aajmIvAFLwwxaymWjWLB1Ig1CSb3XfZChUdMNSg7dozFmm+5MaZerVstmVf0HhcqSn0GJnEuzJBOSWl373KMDym/4FMi2/dzG4KUMr3FyT2mbBLu3NwlgRNdepQ8b9iSrVne2zCLIsCdDVv6TWaYRutk4="""
+
+class ips:
+    def __init__(self, ins_id='',pri='', pub=''):
+        self.id = ins_id
+        self.private = pri
+        self.public = pub
+
+
+class instances:
+    def __init__(self, num_datanodes):
+        self.web = ips()
+        self.mysql = ips()
+        self.mongo = ips()
+        self.namenode = ips()
+        self.datanodes = []
+        for _ in range(num_datanodes):
+            self.datanodes.append(ips())
+
+
+CRED = """aws_access_key_id=ASIAQ6R4KCM7RHA7B67V
+aws_secret_access_key=3H1EHFfMs+EK4CdGsc8lZW3NZfn9nmfsiFJurMFb
+aws_session_token=FwoGZXIvYXdzECgaDD7rgtJw9UCineIe7SLOAVomEfwWAByqR76QOPSQcMrpSqqfsLerrCubWFkKCOfrK4qGq7rXRYvu8/FFtuWRZQsP0LPFN3FOzE/T43KzCbeEedZvEoZ6a8OygU7KOh0H36v+itgzMj+Pq1EIW2q8WQnaqvEazlXq79jSbES2WYPjEor4lN9fIZh59zdBUrnSCKG5duWkVwKizGZuaVq8DIjyFUa9j34CS9XWLD03RE335a3Lr3JgTp0LZb3NMrXbpPGsKbcV5nV0YAYHtTR/kP2iV7f6BUskNLCyDiOMKPvjnP4FMi1z9GTmsW4wkrf5v07wd9fGCSr52LS6If4UYA+GIzzlfENaqIJwh/S5dOCXAFY="""
 IMAGEID = "ami-0f82752aa17ff8f5d"
 G15_SSH_KEY = "g15key"
-G15_SG_WEB = "g15_web_sg" # WEB security group
+G15_SG_WEB = "g15_web_sg"  # WEB security group
 G15_SG_WEB_DESC = "for web"
-G15_SG_MONGO = "g15_mongodb_sg" # mongodb security group
+G15_SG_MONGO = "g15_mongodb_sg"  # mongodb security group
 G15_SG_MONGO_DESC = "for mongodb"
-G15_SG_MYSQL = "g15_mysql_sg" # mysql security group
+G15_SG_MYSQL = "g15_mysql_sg"  # mysql security group
 G15_SG_MYSQL_DESC = "for mysql"
-G15_SG_HADOOP = "g15_hadoop_sg" # hadoop security group
+G15_SG_HADOOP = "g15_hadoop_sg"  # hadoop security group
 G15_SG_HADOOP_DESC = "for hadoop"
 G15_SG_ID = sg_ids()
 G15_VPC_ID = ''
+G15_INSTANCE = {}
+
+G15_INSTANCE_TYPE = {1: "t2.micro", 2: 't2.small',
+                     3: 't2.medium', 4: 't2.large', 5: "t2.xlarge"}
+G15_SELECT_ASK = ''
--- g15automation.py
+++ g15automation.py
@@ -1,13 +1,18 @@
+from os import truncate
+from pickle import NONE
 import boto3
 from g15_config import *
 import traceback
+import os
+import pickle
+import time
 
 
 def process_CRED(somecred):
     """
     Process pasted aws educate CLI credentials from labs.vocareum.com
     Note that this credential's validity is only 3 hours
-    Return a list [aws_access_key_id,aws_secret_access_key,aws_session_token]
+    Return a list [aws_access_key_id,aws_secret_access_key,aws_session_token]g15ec2
     """
     temp = somecred.split('\n')
     result = []
@@ -34,64 +39,155 @@ def create_ssh_key():
     return g15_ssh_key_res
 
 
+def store_instance_ip(instance_id, instance_name):
+    filters = [
+        {'Name': 'instance-id', 'Values': [instance_id]}
+    ]
+    while True:
+        response = list(g15ec2.instances.filter(Filters=filters))
+        try:
+            pub = response[0].public_ip_address
+            pri = response[0].public_ip_address
+            if pub is not None:
+                G15_INSTANCE[instance_name] = {'id': instance_id,
+                                               'public_ip': pub,
+                                               'private_ip': pri}
+                break
+        except:
+            pass
+        time.sleep(2)
+
+
+def select_instance_type(ins_name):
+    global G15_SELECT_ASK
+    if G15_SELECT_ASK == '':
+        # initial ask string
+        temp = f'Please select which instance type for {ins_name} (default t2.medium):\n'
+        for k, v in G15_INSTANCE_TYPE.items():
+            temp += f'({k}) {v}\t'
+        temp += '\nPlease input an integer index:'
+        G15_SELECT_ASK = temp
+    while True:
+        selection = input(G15_SELECT_ASK)
+        try:
+            if selection == '':
+                selection = 3
+                break
+            selection = int(selection)
+            break
+        except:
+            print('Please inout an integer.')
+    return G15_INSTANCE_TYPE[selection]
+
+
 def create_security_group():
     global G15_VPC_ID
     global G15_SG_ID
-    client_ec2 = g15session.client('ec2')
-    vpc_response = client_ec2.describe_vpcs()
+    vpc_response = g15ec2client.describe_vpcs()
     G15_VPC_ID = vpc_response["Vpcs"][0]["VpcId"]
+    # test
+    if os.path.exists('sg_ids'):
+        G15_SG_ID = pickle.load(open('sg_ids', 'rb'))
+    ##
     # create security groups
     try:
         """initial all 4 secuurity groups first"""
-        web_sg_res = client_ec2.create_security_group(GroupName=G15_SG_WEB,
-                                                      Description=G15_SG_WEB_DESC,
-                                                      VpcId=G15_VPC_ID)
+        web_sg_res = g15ec2client.create_security_group(GroupName=G15_SG_WEB,
+                                                        Description=G15_SG_WEB_DESC,
+                                                        VpcId=G15_VPC_ID)
         G15_SG_ID.web = web_sg_res['GroupId']  # WEB
 
-        mongo_sg_res = client_ec2.create_security_group(GroupName=G15_SG_MONGO,
-                                                        Description=G15_SG_MONGO_DESC,
-                                                        VpcId=G15_VPC_ID)
+        mongo_sg_res = g15ec2client.create_security_group(GroupName=G15_SG_MONGO,
+                                                          Description=G15_SG_MONGO_DESC,
+                                                          VpcId=G15_VPC_ID)
         G15_SG_ID.mongo = mongo_sg_res['GroupId']  # MONGODB
 
-        mysql_sg_res = client_ec2.create_security_group(GroupName=G15_SG_MYSQL,
-                                                        Description=G15_SG_MYSQL_DESC,
-                                                        VpcId=G15_VPC_ID)
+        mysql_sg_res = g15ec2client.create_security_group(GroupName=G15_SG_MYSQL,
+                                                          Description=G15_SG_MYSQL_DESC,
+                                                          VpcId=G15_VPC_ID)
         G15_SG_ID.mysql = mysql_sg_res['GroupId']  # MYSQL
 
-        web_hadoop_res = client_ec2.create_security_group(GroupName=G15_SG_HADOOP,
-                                                          Description=G15_SG_HADOOP_DESC,
-                                                          VpcId=G15_VPC_ID)
+        web_hadoop_res = g15ec2client.create_security_group(GroupName=G15_SG_HADOOP,
+                                                            Description=G15_SG_HADOOP_DESC,
+                                                            VpcId=G15_VPC_ID)
         G15_SG_ID.hadoop = web_hadoop_res['GroupId']  # HADOOP
         """Add IP permissions"""
         #########################################
         # WEB security group
-        res = client_ec2.authorize_security_group_ingress(GroupId=G15_SG_ID.web,
-                                                          IpPermissions=[
-                                                              {'IpProtocol': 'tcp',
-                                                               'FromPort': 80,
-                                                               'ToPort': 80,
-                                                               'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-                                                              {'IpProtocol': 'tcp',
-                                                               'FromPort': 22,
-                                                               'ToPort': 22,
-                                                               'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-                                                          ])
+        res = g15ec2client.authorize_security_group_ingress(GroupId=G15_SG_ID.web,
+                                                            IpPermissions=[
+                                                                {'IpProtocol': 'tcp',
+                                                                 'FromPort': 80,
+                                                                 'ToPort': 80,
+                                                                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                                                                {'IpProtocol': 'tcp',
+                                                                    'FromPort': 22,
+                                                                    'ToPort': 22,
+                                                                    'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+                                                            ])
         #########################################
         # mongodb security group
-        res = client_ec2.authorize_security_group_ingress(GroupId=G15_SG_ID.mongo,
-                                                          IpPermissions=[
-                                                              {'IpProtocol': 'tcp',
-                                                               'FromPort': 27017,
-                                                               'ToPort': 27017,
-                                                               'UserIdGroupPairs': [
-                                                                   {
-                                                                       'Description': 'intranet connectivity',
-                                                                       'GroupId': G15_SG_ID.web,
-                                                                   },
-                                                               ], },
-                                                          ])
+        res = g15ec2client.authorize_security_group_ingress(GroupId=G15_SG_ID.mongo,
+                                                            IpPermissions=[
+                                                                {'IpProtocol': 'tcp',
+                                                                 'FromPort': 27017,
+                                                                 'ToPort': 27017,
+                                                                 'UserIdGroupPairs': [
+                                                                     {
+                                                                         'Description': 'intranet connectivity from mongo to web',
+                                                                         'GroupId': G15_SG_ID.web,
+                                                                     },
+                                                                     {
+                                                                         'Description': 'intranet connectivity from mongo to hadoop',
+                                                                         'GroupId': G15_SG_ID.hadoop,
+                                                                     },
+                                                                 ], },
+                                                            ])
         #########################################
-
+        # mysql security group
+        res = g15ec2client.authorize_security_group_ingress(GroupId=G15_SG_ID.mysql,
+                                                            IpPermissions=[
+                                                                {'IpProtocol': 'tcp',
+                                                                 'FromPort': 3306,
+                                                                 'ToPort': 3306,
+                                                                 'UserIdGroupPairs': [
+                                                                     {
+                                                                         'Description': 'intranet connectivity from mysql to web',
+                                                                         'GroupId': G15_SG_ID.web,
+                                                                     },
+                                                                     {
+                                                                         'Description': 'intranet connectivity from mysql to hadoop',
+                                                                         'GroupId': G15_SG_ID.hadoop,
+                                                                     },
+                                                                 ], },
+                                                            ])
+        #########################################
+        # hadoop security group
+        res = g15ec2client.authorize_security_group_ingress(GroupId=G15_SG_ID.hadoop,
+                                                            IpPermissions=[
+                                                                {'IpProtocol': 'tcp',
+                                                                 'FromPort': 9000,
+                                                                 'ToPort': 9000,
+                                                                 'UserIdGroupPairs': [
+                                                                     {
+                                                                         'Description': 'intranet connectivity in hadoop cluster',
+                                                                         'GroupId': G15_SG_ID.hadoop,
+                                                                     },
+                                                                 ], },
+                                                                {'IpProtocol': 'tcp',
+                                                                    'FromPort': 9866,
+                                                                    'ToPort': 9866,
+                                                                    'UserIdGroupPairs': [
+                                                                        {
+                                                                            'Description': 'intranet connectivity in hadoop cluster',
+                                                                            'GroupId': G15_SG_ID.hadoop,
+                                                                        },
+                                                                    ], },
+                                                            ])
+        #########################################
+        fp = open('sg_ids', 'wb')
+        pickle.dump(G15_SG_ID, fp)
+        fp.close()
     except Exception as e:
         traceback.print_exc()
 
@@ -99,6 +195,7 @@ def create_security_group():
 # initialize session and ec2
 g15session = get_aws_session()
 g15ec2 = g15session.resource('ec2')
+g15ec2client = g15session.client('ec2')
 
 # initialized ssh key
 create_ssh_key()
@@ -106,7 +203,24 @@ create_ssh_key()
 # initialize VPC and security groups
 create_security_group()
 
+# create WEB instance
+webtype = select_instance_type("WEB")
+g15_ins_web = g15ec2.create_instances(ImageId=IMAGEID, MinCount=1, MaxCount=1,
+                                      InstanceType=webtype, KeyName=G15_SSH_KEY,
+                                      SecurityGroupIds=[G15_SG_ID.web])
 # create MySQL instance
+mysqltype = select_instance_type("MySQL")
 g15_ins_mysql = g15ec2.create_instances(ImageId=IMAGEID, MinCount=1, MaxCount=1,
-                                        InstanceType='t2.micro', KeyName=G15_SSH_KEY)
-print(g15_ins_mysql)
+                                        InstanceType=mysqltype, KeyName=G15_SSH_KEY,
+                                        SecurityGroupIds=[G15_SG_ID.mysql])
+# create MongoDB instance
+mongotype = select_instance_type("MongoDB")
+g15_ins_mongo = g15ec2.create_instances(ImageId=IMAGEID, MinCount=1, MaxCount=1,
+                                        InstanceType=mongotype, KeyName=G15_SSH_KEY,
+                                        SecurityGroupIds=[G15_SG_ID.mongo])
+# print(g15_ins_mysql[0].id)
+# store_instance_ip(g15_ins_mysql[0].id,'mysql')
+
+store_instance_ip(g15_ins_mysql[0].id,'mysql')
+store_instance_ip(g15_ins_mysql[0].id,'mysql')
+store_instance_ip(g15_ins_mysql[0].id,'mysql')
