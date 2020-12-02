5b383ce76a94af886424f6f18a8db8422bd1d62a
Low Entropy Body <45514917+sutdorg@users.noreply.github.com>
2020-12-02 20:49:21 +0800

    update frontend

--- frontend/websetup.bash
+++ frontend/websetup.bash
@@ -1,4 +1,4 @@
-sudo apt update-y 
+sudo apt update -y 
 sudo apt remove python3 -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
@@ -11,5 +11,3 @@ wget https://bootstrap.pypa.io/get-pip.py
 python3.7 get-pip.py
 rm get-pip.py -f
 pip3 install fastapi uvicorn sqlalchemy pymongo pymysql
-python3.7 -m uvicorn comm_db:app --no-access-log&
-python3.7 -m http.server 2333
